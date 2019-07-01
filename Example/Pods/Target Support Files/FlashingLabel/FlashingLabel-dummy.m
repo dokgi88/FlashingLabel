4bdc024ff74aa1947c4496263a4d7bb54d5bd244
soom <dokgi88@gmail.com>
2019-07-01 22:51:44 +0900

    initial commit

---
+++ Example/Pods/Target Support Files/FlashingLabel/FlashingLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FlashingLabel : NSObject
+@end
+@implementation PodsDummy_FlashingLabel
+@end
---
+++ Example/Pods/Target Support Files/Pods-FlashingLabel_Example/Pods-FlashingLabel_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlashingLabel_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FlashingLabel_Example
+@end
