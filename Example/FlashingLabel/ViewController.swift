1b0f199a3daa345bf33cd0dbd62710c562afac67
soom <dokgi88@gmail.com>
2019-07-03 17:59:53 +0900

    Update Example/ViewController

--- Example/FlashingLabel/ViewController.swift
+++ Example/FlashingLabel/ViewController.swift
@@ -11,9 +11,34 @@ import FlashingLabel
 
 class ViewController: UIViewController {
 
+    private let whiteView: FlashingContainerView = {
+        let view = FlashingContainerView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    private let blackView: FlashingContainerView = {
+        let view = FlashingContainerView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.addSubview(whiteView)
+        view.addSubview(blackView)
+
+        whiteView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        whiteView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        whiteView.trailingAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        whiteView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        blackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        blackView.leadingAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        blackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        blackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +48,123 @@ class ViewController: UIViewController {
 
 }
 
+class FlashingContainerView: UIView {
+    
+    private let flashLabelFirst: FlashingLabel = {
+        let label = FlashingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.baseColor = .orange(0.5)
+        label.flashingColors = [.orange()]
+        label.flashingTime = 0.17
+        label.text = "HELLO"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.startFlashing()
+        return label
+    }()
+    private let flashLabelSecond: FlashingLabel = {
+        let label = FlashingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.baseColor = .orange()
+        label.flashingColors = [.orange(0.5)]
+        label.flashingTime = 0.17
+        label.text = "HELLO"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.startFlashing()
+        return label
+    }()
+    private let flashLabelThird: FlashingLabel = {
+        let label = FlashingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.baseColor = .pantone_serenity()
+        label.flashingColors = [.pantone_serenity(0.5)]
+        label.flashingTime = 0.17
+        label.text = "HELLO"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.startFlashing()
+        return label
+    }()
+    private let flashLabelFourth: FlashingLabel = {
+        let label = FlashingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.baseColor = .pantone_serenity(0.5)
+        label.flashingColors = [.pantone_serenity()]
+        label.flashingTime = 0.17
+        label.text = "HELLO"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.startFlashing()
+        return label
+    }()
+    private let flashLabelMultiple: FlashingLabel = {
+        let label = FlashingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.baseColor = UIColor.black.withAlphaComponent(0.1)
+        label.flashingColors = [.pantone_roseQuartz(), .pantone_serenity(), .pantone_greenery(), .pantone_ultraViolet(), .pantone_livingCoral(), .pantone_roseQuartz()]
+        label.flashingTime = 0.17
+        label.text = "HELLO"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.startFlashing()
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(flashLabelFirst)
+        addSubview(flashLabelSecond)
+        addSubview(flashLabelMultiple)
+        addSubview(flashLabelThird)
+        addSubview(flashLabelFourth)
+        layout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func layout() {
+        flashLabelFirst.bottomAnchor.constraint(equalTo: flashLabelSecond.topAnchor, constant: -8).isActive = true
+        flashLabelFirst.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        flashLabelSecond.bottomAnchor.constraint(equalTo: flashLabelMultiple.topAnchor, constant: -8).isActive = true
+        flashLabelSecond.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        flashLabelMultiple.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        flashLabelMultiple.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        flashLabelThird.topAnchor.constraint(equalTo: flashLabelMultiple.bottomAnchor, constant: 8).isActive = true
+        flashLabelThird.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        flashLabelFourth.topAnchor.constraint(equalTo: flashLabelThird.bottomAnchor, constant: 8).isActive = true
+        flashLabelFourth.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+    
+}
+
+extension UIColor {
+    
+    class func orange(_ alpha: CGFloat? = nil) -> UIColor {
+        let a: CGFloat = alpha == nil ? 1:alpha!
+        return UIColor.orange.withAlphaComponent(a)
+    }
+    class func pantone_roseQuartz(_ alpha: CGFloat? = nil) -> UIColor {
+        let a: CGFloat = alpha == nil ? 1:alpha!
+        return UIColor(red: 247/255, green: 202/255, blue: 201/255, alpha: a)
+    }
+    class func pantone_serenity(_ alpha: CGFloat? = nil) -> UIColor {
+        let a: CGFloat = alpha == nil ? 1:alpha!
+        return UIColor(red: 145/255, green: 168/255, blue: 209/255, alpha: a)
+    }
+    class func pantone_greenery(_ alpha: CGFloat? = nil) -> UIColor {
+        let a: CGFloat = alpha == nil ? 1:alpha!
+        return UIColor(red: 136/255, green: 176/255, blue: 75/255, alpha: a)
+    }
+    class func pantone_ultraViolet(_ alpha: CGFloat? = nil) -> UIColor {
+        let a: CGFloat = alpha == nil ? 1:alpha!
+        return UIColor(red: 95/255, green: 75/255, blue: 139/255, alpha: a)
+    }
+    class func pantone_livingCoral(_ alpha: CGFloat? = nil) -> UIColor {
+        let a: CGFloat = alpha == nil ? 1:alpha!
+        return UIColor(red: 255/255, green: 111/255, blue: 97/255, alpha: a)
+    }
+    
+}
