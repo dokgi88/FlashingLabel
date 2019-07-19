b060764f6014673667248475c1bb2e6e13e38adf
soom <dokgi88@gmail.com>
2019-07-19 10:10:09 +0900

    Add infinite feature to FlashingLabel

--- FlashingLabel/Classes/FlashingLabel.swift
+++ FlashingLabel/Classes/FlashingLabel.swift
@@ -12,10 +12,17 @@ public class FlashingLabel: UILabel {
     public var baseColor: UIColor?
     public var flashingColors: [UIColor]?
     public var flashingTime: TimeInterval?
+    public var isInfinite: Bool = false
     
     private var isDraw = false
     private var flashingCount = 0
     private var isFlashing = true
+    private var textCount: Int {
+        guard let text = text else {return 0}
+        return Array(text).count
+    }
+    
+    private lazy var infiniteColors = [UIColor]()
     private lazy var flashingTimer = Timer()
 
     public override func draw(_ rect: CGRect) {
@@ -39,27 +46,42 @@ public class FlashingLabel: UILabel {
         guard let baseColor = baseColor else {return}
         textColor = baseColor
         
-        guard !flashingTimer.isValid, let flashingTime = flashingTime else {return}
-        flashingTimer = Timer.scheduledTimer(timeInterval: flashingTime, target: self, selector: #selector(repeatFlashing), userInfo: nil, repeats: true)
+        guard !flashingTimer.isValid, let flashingTime = flashingTime, let flashingColors = flashingColors else {return}
+        
+        var tempColors = [UIColor]()
+        
+        defer {
+            flashingTimer = Timer.scheduledTimer(timeInterval: flashingTime, target: self, selector: #selector(repeatFlashing(_:)), userInfo: tempColors, repeats: true)
+        }
+        
+        guard isInfinite, textCount > flashingColors.count else {
+            let colorsLastIndex = (flashingColors.count > textCount) ? textCount-1:flashingColors.count-1
+            tempColors = flashingColors[0...colorsLastIndex].map{$0}
+            return
+        }
+        
+        var colorCount = 0
+        while tempColors.count < textCount {
+            tempColors.append(flashingColors[colorCount])
+            colorCount = (colorCount+1) == flashingColors.count ? 0:colorCount+1
+        }
     }
     
-    @objc private func repeatFlashing() {
-        guard let flashingColors = flashingColors, let text = text else {return}
-        let colorsLastIndex = (flashingColors.count > Array(text).count) ? Array(text).count-1:flashingColors.count-1
-        let sliceColors = flashingColors[0...colorsLastIndex].map{$0}
+    @objc private func repeatFlashing(_ timer: Timer) {
+        guard let colors = timer.userInfo as? [UIColor], let text = text else {return}
         
         let attribute = NSMutableAttributedString(string: text)
-        for i in 0..<sliceColors.count {
+        for i in 0..<colors.count {
             let increCount = flashingCount+i
-            let count = (Array(text).count <= increCount) ? increCount-Array(text).count:increCount
-            let color = sliceColors[i]
+            let count = (textCount <= increCount) ? increCount-textCount:increCount
+            let color = colors[i]
             let range = NSMakeRange(count, 1)
             attribute.addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: range)
         }
         attributedText = attribute
         
         flashingCount += 1
-        flashingCount = (Array(text).count == flashingCount) ? 0:flashingCount
+        flashingCount = (textCount == flashingCount) ? 0:flashingCount
     }
 
 }
